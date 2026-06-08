7da79e5b7e5cb79fc7c27cfaac8f654fa4f7256e
IsmailHamdan-007 <ismailhamdank@gmail.com>
2026-06-08 18:15:47 +0530

    Update 02_revenue_analysis.sql

--- SQL/02_revenue_analysis.sql
+++ SQL/02_revenue_analysis.sql
@@ -1,11 +1,11 @@
 USE SupplychainDB;
 
--- Total Revenue
+-- 1. Total Revenue
 SELECT 
 	SUM(Sales) AS Total_Revenue
 FROM sales;
 
--- Monthly Revenue Trend
+-- 2. Monthly Revenue Trend
 SELECT
 	YEAR(O.Order_Date) AS Years,
 	MONTH(O.Order_Date) AS Months,
@@ -17,7 +17,7 @@ GROUP BY YEAR(O.Order_Date),
 	MONTH(O.Order_Date)
 ORDER BY Years, Months;
 
--- Revenue by Market
+-- 3. Revenue by Market
 SELECT 
 	O.Market,
 	SUM(S.Sales) AS Revenue
@@ -27,7 +27,7 @@ ON S.Order_Id = O.Order_Id
 GROUP BY O.Market
 ORDER BY Revenue;
 
--- Revenue by Category
+-- 4. Revenue by Category
 SELECT 
 	P.Category_Name,
 	SUM(S.Sales) AS Revenue
@@ -37,7 +37,7 @@ ON S.Product_Card_Id = P.Product_Card_Id
 GROUP BY P.Category_Name
 ORDER BY Revenue;
 
--- Revenue by Department
+-- 5. Revenue by Department
 SELECT 
 	P.Department_Name,
 	SUM(S.Sales) AS Revenue
@@ -47,7 +47,7 @@ ON S.Product_Card_Id = P.Product_Card_Id
 GROUP BY P.Department_Name
 ORDER BY Revenue;
 
--- Top Revenue Products
+-- 6. Top Revenue Products
 WITH ProductRevenue AS
 (
     SELECT
@@ -66,7 +66,7 @@ SELECT *
 FROM ProductRevenue
 WHERE rn <= 3;
 
--- Top Revenue Customers
+-- 7. Top Revenue Customers
 SELECT TOP 3
 	C.Customer_Name,
 	SUM(S.Sales) AS Revenue,
@@ -77,7 +77,7 @@ INNER JOIN customers C
 ON S.Customer_Id = C.Customer_Id
 GROUP BY C.Customer_Name;
 
--- Revenue Contribution by Market
+-- 8. Revenue Contribution by Market
 SELECT 
 	O.Market,
 	SUM(S.Sales) AS Revenue,
@@ -89,6 +89,78 @@ INNER JOIN orders O
 ON S.Order_Id = O.Order_Id
 GROUP BY O.Market
 ORDER BY Revenue;
+
+-- 9. Revenue Contribution % by Category
+SELECT 
+	P.Category_Name,
+	SUM(S.Sales) AS Revenue,
+	ROUND(SUM(S.Sales) * 100.00 /
+		SUM(SUM(S.Sales)) OVER(), 2)
+	AS Contribution_Percent
+FROM sales S
+INNER JOIN products P
+ON S.Product_Card_Id = P.Product_Card_Id
+GROUP BY P.Category_Name;
+
+-- 10. Revenue Contribution % by Department
+SELECT 
+	P.Department_Name,
+	SUM(S.Sales) AS Revenue,
+	ROUND(SUM(S.Sales) * 100.00 /
+		SUM(SUM(S.Sales)) OVER(), 2)
+	AS Contribution_Percent
+FROM sales S
+INNER JOIN products P
+ON S.Product_Card_Id = P.Product_Card_Id
+GROUP BY P.Department_Name;
+
+-- 11. Average Revenue per Customer
+WITH avg_rev_per_cust AS
+(
+SELECT 
+	SUM(S.Sales) AS Revenue,
+	COUNT(DISTINCT C.Customer_Id) AS Counts
+FROM sales S
+INNER JOIN customers C
+ON S.Customer_Id = C.Customer_Id
+GROUP BY C.Customer_Id
+)
+SELECT 
+	AVG(Revenue) AS avg_rev_per_cust
+FROM avg_rev_per_cust;
+
+-- 12. Average Revenue per Order
+WITH avg_ord AS
+(
+SELECT 
+	O.Order_Id,
+	SUM(S.Sales) AS Revenue
+FROM sales S
+INNER JOIN orders O
+ON S.Order_Id = O.Order_Id
+GROUP BY O.Order_Id
+)
+SELECT 
+	AVG(Revenue) AS avg_rev_per_ord
+FROM avg_ord;
+
+-- 13.Revenue per Market per Month
+SELECT 
+	YEAR(O.Order_Date) AS Years,
+	MONTH(O.Order_Date) AS Months,
+	SUM(S.Sales) AS Revenue,
+	O.Market
+FROM sales S
+INNER JOIN orders O
+ON S.Order_Id = O.Order_Id
+GROUP BY YEAR(O.Order_Date), MONTH(O.Order_Date),
+	O.Market
+ORDER BY Years, Months;
+
+
+
+
+
 
 
 
